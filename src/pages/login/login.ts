92a56eb96f377211f4dcf2a7ff5c149bcf461891
Alan Araujo <alan.techsafe@hotmail.com>
2018-09-02 18:53:59 -0300

    Validação do retorno da api e Toast

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 import { LoginProvider } from '../../providers/login/login';
 import { Funcionario } from '../../model/orcamento';
@@ -11,14 +11,16 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'login.html',
 })
 export class LoginPage {
- login = new Funcionario;
+
+ public login = new Funcionario();
 
   constructor(
     public storage: Storage, 
     public navCtrl: NavController, 
     public navParams: NavParams, 
     private loginProvider: LoginProvider,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    public toast: ToastController
   ) {
   }
 
@@ -33,18 +35,26 @@ export class LoginPage {
   
   }
 
-  logar() {  
+  logar(_login) { 
     let load = this.loadingCtrl.create({content:"Aguarde um momento..."});
     load.present();  
-    this.loginProvider.logar(this.login).toPromise().then(res=>{
+    this.loginProvider.logar(_login).toPromise().then(res=>{
       load.dismiss();
-      if(res.Error) alert(res.Error);
+      console.log(res);
+      if(res.nome==null) {
+        this.toast.create({message:"Usuário ou senha incorretos.", duration:3000, position:"botton"}).present();
+      }
       else{
         this.login = res.Funcionario; 
         this.loginProvider.setUsuario(res.Funcionario);
         this.navCtrl.setRoot(HomePage);
       }
       }
-    );
+    ).catch(
+      ()=>{
+        load.dismiss();
+        this.toast.create({message:"Usuário ou senha incorretos.", duration:3000, position:"botton"}).present();
+      }
+  );
   }
 }
